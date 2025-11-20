4104e53ca326a0731ffbce906011da24b9f953d5
JCroc25 <jcrocdx@gmail.com>
2025-11-20 23:54:22 +0000

    Added Submit Function

--- Subpages/forms-script.js
+++ Subpages/forms-script.js
@@ -1,6 +1,6 @@
 var audio = new Audio('../Audio/New.wav');
 audio.play();
-audio.volume = 0.5;
+audio.volume = 0.25;
 audio.addEventListener('ended', function () {
     this.currentTime = 0;
     this.play();
@@ -43,15 +43,13 @@ SUBMIT.addEventListener("click", submitClick);
 
 function submitClick(event) {
     event.preventDefault();
+    let counter = 0;
     let firstName = document.getElementById('fname').value;
     let lastName = document.getElementById('lname').value;
     let birthDate = document.getElementById('bdate').value;
     let favColor = document.getElementById('favcolor').value;
 
     console.info(firstName + " " + lastName + " " + birthDate + " " + favColor);
-}
-
-function ulli() {
     const DIV = document.createElement('div');
     DIV.setAttribute('class', 'object');
     
@@ -59,27 +57,27 @@ function ulli() {
 
     const PFNAME = document.createElement('p');
     PFNAME.setAttribute('class', 'firstName');
-    PFNAME.textContent="First Name: ";
+    PFNAME.textContent="First Name: " + firstName;
 
     const PLNAME = document.createElement('p');
     PLNAME.setAttribute('class', 'lastName');
-    PLNAME.textContent="Last Name: ";
+    PLNAME.textContent="Last Name: " + lastName;
 
     const PBDATE = document.createElement('p');
     PBDATE.setAttribute('class', 'birthDate');
-    PBDATE.textContent="Date of Birth: ";
+    PBDATE.textContent="Date of Birth: " + birthDate;
 
     const PFAVCOLOR = document.createElement('p');
     const OBJCOLOR = document.createElement('span');
     OBJCOLOR.setAttribute('class', 'objColor');
     PFAVCOLOR.setAttribute('class', 'favColor');
-    PFAVCOLOR.textContent="Favorite Color: ";
-
-
+    PFAVCOLOR.textContent="Favorite Color: " + favColor;
+    
+    
     DIV.appendChild(PFNAME);
     DIV.appendChild(PLNAME);
     DIV.appendChild(PBDATE);
     DIV.appendChild(PFAVCOLOR);
     PFAVCOLOR.appendChild(OBJCOLOR);
+    OBJCOLOR.style.backgroundColor = favColor;
 }
-ulli();
